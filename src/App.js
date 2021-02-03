91e544795d650f0f1a5444233f2abfd9f17c3b67
John Arnold <johnadamarnold@gmail.com>
2021-02-03 10:57:34 +0000

    add ga

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react"
-import "./index.css"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import { CssBaseline, Container } from "@material-ui/core"
+import React from 'react'
+import ReactGA from 'react-ga'
 
-import Header from "./components/Header"
-import Home from "./pages/Home"
-import About from "./pages/About"
+import './index.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { CssBaseline, Container } from '@material-ui/core'
+
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
 
 export default function App() {
+  ReactGA.initialize('UA-188877942-1')
+  ReactGA.pageview('/')
+
   return (
     <BrowserRouter>
       <CssBaseline />
-      <Container maxWidth="md">
+      <Container maxWidth='md'>
         <Header />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
         </Switch>
       </Container>
     </BrowserRouter>
